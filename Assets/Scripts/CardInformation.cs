bb1c6dafa85b6cbe2f6c8c12f2febc6b16707e94
BrunoCooper17 <BrunoCooper17@outlook.com>
2017-02-14 17:53:24 -0600

    Added a stabilizer for the info cards

--- Assets/Scripts/CardInformation.cs
+++ Assets/Scripts/CardInformation.cs
@@ -17,6 +17,30 @@ public class CardInformation : MonoBehaviour {
 
     public ID_CARD Id_Card;
 
+    public Animator animator;
+
+    public Vector3 tmpPositionCursor;
+    public float alpha = 0.95f;
+    public GameObject cursorLocation;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        cursorLocation = GameObject.Find("GazeCursor");
+    }
+
+    void Update()
+    {
+        tmpPositionCursor = (alpha * (cursorLocation.transform.position.normalized)) + (1.0f - alpha) * tmpPositionCursor;
+
+        if (animator.GetCurrentAnimatorClipInfo(0)[0].clip.name == "Card_Show" && (animator.GetCurrentAnimatorStateInfo(0).length*2 <
+            animator.GetCurrentAnimatorStateInfo(0).normalizedTime))
+        {
+            animator.Stop();
+            transform.position = tmpPositionCursor;
+        }
+    }
+
     public void PlayShow()
     {
         GetComponent<Animator>().Play("Card_Show", 0, 0.0f);
